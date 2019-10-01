b870b9204fa528caa95a084f01485cc27bb15237
GlebChili <gleb.chili@gmail.com>
2019-10-02 01:12:41 +0300

    Working proof of concept net core integration was implemented
    Working unloading and cleanup with NET Core 3.0 hostfxr API was implemented. Working NET Core context reinitialization was implemented. hostfxr.h header from dotnet/core-setup was added.

---
+++ gm_dotnet_managed/GmodNET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace GmodNET
+{
+    public delegate void MainDelegate();
+    public static class Startup
+    {
+        public static void Main()
+        {
+           File.WriteAllText("ManagedGeneratedFile.txt", "Managed code says hi!");
+        }
+    }
+}
--- gm_dotnet_native/src/gm_dotnet.cpp
+++ gm_dotnet_native/src/gm_dotnet.cpp
@@ -1,122 +1,115 @@
 #include<GarrysMod/Lua/Interface.h>
 #include<GarrysMod/Lua/LuaBase.h>
+#include<netcore/hostfxr.h>
+#include <codecvt>
 #ifdef WIN32
 #include <Windows.h>
 #include <direct.h>
 #else
 #include <dlfcn.h>
 #include <unistd.h>
+#include<locale>
 #endif // WIN32
 #include <string>
-
 
 using namespace std;
 using namespace GarrysMod::Lua;
 
-//Define pointers for the CoreCLR hosting
+wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
 
-//Pointer to the coreclr_initialize function
-typedef int (*coreclr_initialize_ptr)(const char* exePath, const char* AppDomainFriendlyName, int PropertyCount, const char** propertyKeys,
-	const char** propertyValues, void** hostHandle, unsigned int* domainId);
+hostfxr_initialize_for_runtime_config_fn hostfxr_initialize_for_runtime_config = nullptr;
+hostfxr_handle host_fxr_handle = nullptr;
+hostfxr_get_runtime_delegate_fn hostfxr_get_runtime_delegate = nullptr;
+hostfxr_close_fn hostfxr_close = nullptr;
 
-//Pointer to the coreclr_shutdown function
-typedef int (*coreclr_shutdown_ptr)(void* hostHandle, unsigned int domainId);
-
-//Pointer to the coreclr_create_delegate function
-typedef int (*coreclr_create_delegate_ptr)(void* hostHandle, unsigned int domainId, const char* entryPointAssemblyName, const char* entryPointTypeName,
-	const char* entryPointMethodName, void** delegate);
-
-//Function pointers
-
-coreclr_initialize_ptr coreclr_initialize = nullptr;
-coreclr_shutdown_ptr coreclr_shutdown = nullptr;
-coreclr_create_delegate_ptr coreclr_create_delegate = nullptr;
-
-//Pointer to the CoreCLR library
-void* coreclr_ptr = nullptr;
-
-//Id of the main domain
-unsigned int domainId = 0;
-//Handle of the host;
-void* hostHandle = nullptr;
-
-//Main delegate pointer
-typedef void (*delegate_ptr)();
 
 //Invoked by Garry's Mod on module load
 GMOD_MODULE_OPEN()
 {
-	//Get path to the working directory
-	char* working_path_c_str;
-#ifdef WIN32
-	working_path_c_str = _getcwd(nullptr, 500);
-#else
-	working_path_c_str = getcwd(NULL, 500);
-#endif // WIN32
+    char game_char_buffer [300];
+    #ifdef WIN32
+    int game_path_length = GetModuleFileNameA(nullptr, game_char_buffer, 299);
+    #else
+    int game_path_length = readlink("/proc/self/exe", game_char_buffer, 299);
+    #endif
+    char dotnet_folder [] = "garrysmod/lua/bin/dotnet";
+    hostfxr_initialize_parameters runtime_params = {sizeof(hostfxr_initialize_parameters), (const char_t*)game_char_buffer, (const char_t*)dotnet_folder};
+    void * hostfxr_pointer = nullptr;
+    #ifdef WIN32
+    hostfxr_pointer = LoadLibraryA("garrysmod/lua/bin/dotnet/host/fxr/3.0.0/hostfxr.dll");
+    #endif
+    if(hostfxr_pointer == nullptr)
+    {
+         fprintf(stderr, "Unable to load hostfxr! \n");
+         return 0;
+    }
 
-	string working_path(working_path_c_str);
+    #ifdef WIN32
+    hostfxr_initialize_for_runtime_config = (hostfxr_initialize_for_runtime_config_fn)GetProcAddress((HMODULE)hostfxr_pointer, "hostfxr_initialize_for_runtime_config");
+    #endif
+    if(hostfxr_initialize_for_runtime_config == nullptr)
+    {
+        fprintf(stderr, "Unable to locate hostfxr_initialize_for_runtime_config function!");
+        return 0;
+    }
+    hostfxr_initialize_for_runtime_config(converter.from_bytes("garrysmod/lua/bin/GmodNET.runtimeconfig.json").c_str(), &runtime_params, &host_fxr_handle);
+    if(host_fxr_handle == nullptr)
+    {
+        fprintf(stderr, "Unable to create hostfxr handle!");
+        return 0;
+    }
 
-	free(working_path_c_str);
+    #ifdef WIN32
+    hostfxr_get_runtime_delegate = (hostfxr_get_runtime_delegate_fn)GetProcAddress((HMODULE)hostfxr_pointer, "hostfxr_get_runtime_delegate");
+    #endif
+    if(hostfxr_get_runtime_delegate == nullptr)
+    {
+        fprintf(stderr, "Unable to locate hostfxr_get_runtime_delegate! \n");
+        return 0;
+    }
+    typedef void (*get_function_pointer_fn)(const char_t * assembly_path, const char_t * type_name, const char_t * method_name, const char_t * delegate_type_name,
+            void * reserved, void ** delegate);
+    get_function_pointer_fn get_function_pointer = nullptr;
+    hostfxr_get_runtime_delegate(host_fxr_handle, hdt_load_assembly_and_get_function_pointer, (void **)&get_function_pointer);
+    if(get_function_pointer == nullptr)
+    {
+        fprintf(stderr, "Unable to get get_function_pointer helper! \n");
+        return 0;
+    }
 
-	//Load CoreCLR
-	if (!coreclr_ptr)
-	{
-#ifdef WIN32
-		coreclr_ptr = LoadLibrary((working_path + "/garrysmod/lua/bin/gm_dotnet_core/coreclr.dll").c_str());
-#else
-#ifndef __APPLE__
-		coreclr_ptr = dlopen((working_path + "/garrysmod/lua/bin/gm_dotnet_core/libcoreclr.so").c_str(), RTLD_LAZY);
-#else
-		coreclr_ptr = dlopen((working_path + "/garrysmod/lua/bin/gm_dotnet_core/libcoreclr.dylib").c_str(), RTLD_LAZY);
-#endif
-#endif
-	}
+    typedef void (*managed_delegate_fn)();
+    managed_delegate_fn managed_delegate = nullptr;
+    get_function_pointer(converter.from_bytes("garrysmod/lua/bin/GmodNET.dll").c_str(), converter.from_bytes("GmodNET.Startup, GmodNET").c_str(), converter.from_bytes("Main").c_str(),
+            converter.from_bytes("GmodNET.MainDelegate, GmodNET").c_str(), nullptr, (void**)&managed_delegate);
+    if(managed_delegate == nullptr)
+    {
+        fprintf(stderr, "Unable to get managed delegate! \n");
+        return 0;
+    }
 
-	//Load process symbols
-	if (!coreclr_initialize)
-	{
-#ifdef WIN32
-		coreclr_initialize = (coreclr_initialize_ptr)GetProcAddress((HMODULE)coreclr_ptr, "coreclr_initialize");
-		coreclr_shutdown = (coreclr_shutdown_ptr)GetProcAddress((HMODULE)coreclr_ptr, "coreclr_shutdown");
-		coreclr_create_delegate = (coreclr_create_delegate_ptr)GetProcAddress((HMODULE)coreclr_ptr, "coreclr_create_delegate");
-#else
-		coreclr_initialize = (coreclr_initialize_ptr)dlsym(coreclr_ptr, "coreclr_initialize");
-		coreclr_shutdown = (coreclr_shutdown_ptr)dlsym(coreclr_ptr, "coreclr_shutdown");
-		coreclr_create_delegate = (coreclr_create_delegate_ptr)dlsym(coreclr_ptr, "coreclr_create_delegate");
-#endif
-	}
+    managed_delegate();
 
-	//Create list of app pathes
-#ifdef WIN32
-	string separator = ";";
-#else
-	string separator = ":";
-#endif
-
-	//Form list of runtime properties
-	string list_of_app_paths = working_path + "/garrysmod/lua/bin/gm_dotnet_core/" + separator + working_path + "/garrysmod/lua/bin/GmodDotNet";
-	string list_of_native_app_paths = working_path + "/garrysmod/lua/bin/gm_dotnet_core";
-
-	const char* param_keys[] = { "APP_PATHS", "APP_NI_PATHS" };
-	const char* param_values[] = { list_of_app_paths.c_str(), list_of_native_app_paths.c_str() };
-
-	//Init CoreCLR
-	coreclr_initialize((working_path + "/garrysmod/lua/bin/GmodDotNet").c_str(), "GmodDotNetDomain", 2, param_keys, param_values, &hostHandle, &domainId);
-
-	//Get delegate for the Managed Main method
-	delegate_ptr main = nullptr;
-	coreclr_create_delegate(hostHandle, domainId, "GmodDotNet, Version=0.5.0", "GmodDotNet.GmodDotNet", "Main", (void**)& main);
-
-	//Call delegate
-	main();
-
-	return 0;
+    return 0;
 }
 
-//Invoked by Garry;s Mod on module unload
+//Invoked by Garry's Mod on module unload
 GMOD_MODULE_CLOSE()
 {
-	coreclr_shutdown(hostHandle, domainId);
+    #ifdef WIN32
+    HMODULE hostfxr_lib = LoadLibraryA("garrysmod/lua/bin/dotnet/host/fxr/3.0.0/hostfxr.dll");
+    hostfxr_close = (hostfxr_close_fn)GetProcAddress(hostfxr_lib, "hostfxr_close");
+    #endif
+    if(hostfxr_close == nullptr)
+    {
+        fprintf(stderr, "Unable to load hosfxr_close! \n");
+        return 0;
+    }
+    hostfxr_close(host_fxr_handle);
 
-	return 0;
+    host_fxr_handle = nullptr;
+    hostfxr_initialize_for_runtime_config = nullptr;
+    hostfxr_get_runtime_delegate = nullptr;
+    hostfxr_close = nullptr;
+
+    return 0;
 }
