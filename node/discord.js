ff5ab6cc2bd3f82cbe0863b48d053e3e59f964be
GlebChili <gleb.chili@gmail.com>
2019-11-14 05:07:16 +0300

    node module added

---
+++ node/discord.js
@@ -0,0 +1,17 @@
+let webhook = process.argv[2];
+let branch = process.argv[3];
+let commit = process.argv[4];
+
+const whook = require("webhook-discord");
+
+const hook = new whook.Webhook(webhook);
+
+hook.info("Travis CI", "**Successful build**: " + branch + " " + commit + "\n \n Windows: https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Windows/" + branch + "/" + branch + "-" + commit + ".zip"
+            + "\n\n"
+            + "Linux: " + "https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Linux/" + branch + "/" + branch + "-" + commit + ".tar.gz"
+            + "\n\n"
+            + "Osx: " + "https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Osx/" + branch + "/" + branch + "-" + commit + ".tar.gz"
+            + "\n\n"
+            + "Lua client: " + "https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Lua/" + branch + "/" + commit + "/" + "gm_dotnet_client.lua"
+            + "\n\n"
+            + "Lua server: " + "https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Lua/" + branch + "/" + commit + "/" + "gm_dotnet_server.lua");
