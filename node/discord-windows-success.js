847ef11e253f76ef8de5307bbece073e9b5f2619
GlebChili <gleb.chili@gmail.com>
2019-12-08 23:51:43 +0300

    Windows tester added

---
+++ node/discord-windows-success.js
@@ -0,0 +1,9 @@
+let webhook = process.argv[2];
+let branch = process.argv[3];
+let commit = process.argv[4];
+
+const whook = require("webhook-discord");
+
+const hook = new whook.Webhook(webhook);
+
+hook.success("Travis CI", "**Build successfully passed tests on Windows**: " + branch + " " + commit);
