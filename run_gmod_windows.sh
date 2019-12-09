159b1b558f7c49b7e1d74abb22c51a133a557652
GlebChili <gleb.chili@gmail.com>
2019-12-09 17:57:11 +0300

    I hope it finally works

--- run_gmod_windows.sh
+++ run_gmod_windows.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run your app in the background
-./gmod/srcds_win64.exe -console -systemtest -game "garrysmod" +exec "server.cfg" +gamemode sandbox +map gm_construct +maxplayers 16 &
+powershell -Command './gmod/srcds_win64.exe -console -systemtest -game "garrysmod" +exec "server.cfg" +gamemode sandbox +map gm_construct +maxplayers 16 &'
 
 # Store it's Process ID
 bg_pid=$!
