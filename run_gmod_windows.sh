dca9960a0f0245d182666042e3a43f94e2fcedd4
GlebChili <gleb.chili@gmail.com>
2019-12-09 17:08:05 +0300

    I hate srcds on Windows

---
+++ run_gmod_windows.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run your app in the background
+./gmod/srcds_win64.exe -console -systemtest -game "garrysmod" +exec "server.cfg" +gamemode sandbox +map gm_construct +maxplayers 16 &
+
+# Store it's Process ID
+bg_pid=$!
+
+# sleep for X seconds
+sleep 120
+
+# Kill the python process
+kill $bg_pid
+
+# Optionally exit true to prevent travis seeing this as an error
+exit 0
