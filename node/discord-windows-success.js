30d4120ce0b705ecd97bd8e5c434b7cd3e125c7f
GlebChili <gleb.chili@gmail.com>
2020-01-10 11:51:47 +0300

    Discord notifications update

--- node/discord-windows-success.js
+++ node/discord-windows-success.js
@@ -1,9 +1,10 @@
 let webhook = process.argv[2];
 let branch = process.argv[3];
 let commit = process.argv[4];
+let commit_message = process.argv[5];
 
 const whook = require("webhook-discord");
 
 const hook = new whook.Webhook(webhook);
 
-hook.success("Travis CI", "**Build successfully passed tests on Windows**: " + branch + " " + commit);
+hook.success("Travis CI", "**Build successfully passed tests on Windows**: " + branch + " " + commit + " " + "\"" + commit_message + "\"");
--- node/discord.js
+++ node/discord.js
@@ -1,12 +1,14 @@
 let webhook = process.argv[2];
 let branch = process.argv[3];
 let commit = process.argv[4];
+let commit_message = process.argv[5];
 
 const whook = require("webhook-discord");
 
 const hook = new whook.Webhook(webhook);
 
-hook.info("Travis CI", "**Successful build**: " + branch + " " + commit + "\n \n Windows: https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Windows/" + branch + "/" + branch + "-" + commit + ".zip"
+hook.info("Travis CI", "**Successful build**: " + branch + " " + commit + " " + "\"" + commit_message + "\""
+            + "\n\n Windows: https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Windows/" + branch + "/" + branch + "-" + commit + ".zip"
             + "\n\n"
             + "Linux: " + "https://gleb-krasilich.fra1.digitaloceanspaces.com/GmodDotNetBuilds/Linux/" + branch + "/" + branch + "-" + commit + ".tar.gz"
             + "\n\n"
