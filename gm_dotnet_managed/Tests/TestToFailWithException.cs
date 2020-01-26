d3ca26e04effbc5ac3f3693ae7bd2987d71430ec
GlebChili <gleb.chili@gmail.com>
2020-01-26 03:28:16 +0300

    Test travis CI with TestWithException

---
+++ gm_dotnet_managed/Tests/TestToFailWithException.cs
@@ -0,0 +1,18 @@
+﻿using GmodNET.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class TestToFailWithException : ITest
+    {
+        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor)
+        {
+            TaskCompletionSource<bool> taskCompletion = new TaskCompletionSource<bool>();
+            taskCompletion.TrySetException(new Exception[] {new Exception("This exception will always occur!")});
+            return taskCompletion.Task;
+        }
+    }
+}
