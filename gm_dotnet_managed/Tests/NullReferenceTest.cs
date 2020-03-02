bcd46e944d260cd7c1db5dd415629c0deb46978b
GlebChili <gleb.chili@gmail.com>
2020-03-02 13:42:24 +0300

    NullReferenceException test added

---
+++ gm_dotnet_managed/Tests/NullReferenceTest.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using GmodNET.API;
+
+namespace Tests
+{
+    // This test checks that CoreCLR can catch NullReferenceException (which is important on *nix systems since it uses SIGSEGV handling to detect)
+    public class NullReferenceTest : ITest
+    {
+        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor)
+        {
+            TaskCompletionSource<bool> taskCompletion = new TaskCompletionSource<bool>();
+
+            try
+            {
+                unsafe
+                {
+                    int* ptr = null;
+                    Console.WriteLine(*ptr);
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                // Null reference successfully caught
+                taskCompletion.TrySetResult(true);
+            }
+
+            return taskCompletion.Task;
+        }
+    }
+}
