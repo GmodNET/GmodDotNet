497f905163c7188d997652e2a04add0e8c957030
GlebChili <gleb.chili@gmail.com>
2020-07-25 01:34:52 +0300

    GetTypeNameEnum test was added

---
+++ gm_dotnet_managed/Tests/GetTypeNameEnum.cs
@@ -0,0 +1,48 @@
+﻿using GmodNET.API;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    // Tests ILua.GetTypeName(TYPES) method
+    public class GetTypeNameEnum : ITest
+    {
+        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor)
+        {
+            TaskCompletionSource<bool> taskCompletion = new TaskCompletionSource<bool>();
+
+            try
+            {
+                if(lua.GetTypeName(TYPES.NIL) != "nil")
+                {
+                    throw new Exception("GetTypeName returned incorrect name for the type NIL");
+                }
+
+                if(lua.GetTypeName(TYPES.STRING) != "string")
+                {
+                    throw new Exception("GetTypeName returned incorrect name for the type STRING");
+                }
+
+                if(lua.GetTypeName(TYPES.NUMBER) != "number")
+                {
+                    throw new Exception("GetTypeName returned incorrect name for the type NUMBER");
+                }
+
+                if(lua.GetTypeName(TYPES.BOOL) != "bool")
+                {
+                    throw new Exception("GetTypeName returned incorrect name for the type BOOL");
+                }
+
+                taskCompletion.TrySetResult(true);
+            }
+            catch(Exception e)
+            {
+                taskCompletion.TrySetException(new Exception[] { e });
+            }
+
+            return taskCompletion.Task;
+        }
+    }
+}
