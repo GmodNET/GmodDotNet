d7d5129ae001090ed181004b6f9722dbaf9c5aaa
GlebChili <gleb.chili@gmail.com>
2020-07-31 03:05:10 +0300

    ModuleAssemblyLoadContext docs added

--- gm_dotnet_managed/GmodNET.API/ModuleAssemblyLoadContext.cs
+++ gm_dotnet_managed/GmodNET.API/ModuleAssemblyLoadContext.cs
@@ -5,14 +5,58 @@ using System.Runtime.Loader;
 
 namespace GmodNET.API
 {
+    /// <summary>
+    /// An extension of <see cref="AssemblyLoadContext" /> class with GmodNET specific features.
+    /// </summary>
     public abstract class ModuleAssemblyLoadContext : AssemblyLoadContext
     {
+        /// <summary>
+        /// Get associated module name.
+        /// </summary>
         public abstract string ModuleName {get; }
 
+
+        /// <summary>
+        /// Get current custom native library resolver delegate.
+        /// </summary>
         public abstract Func<ModuleAssemblyLoadContext, string, IntPtr> CustomNativeLibraryResolver {get; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModuleAssemblyLoadContext" /> class with a value that indicates whether unloading is enabled.
+        /// </summary>
+        /// <param name="isCollectible"></param>
         public ModuleAssemblyLoadContext(bool isCollectible) : base(isCollectible: isCollectible){ }
 
+        /// <summary>
+        /// Sets <paramref name="resolver"/> as new custom native library resolution middleware for the current instance of <see cref="ModuleAssemblyLoadContext"/>.
+        /// </summary>
+        /// <param name="resolver">
+        /// A new custom native library resolver delegate. This delegate takes an instance of <see cref="ModuleAssemblyLoadContext"/> in which native resolution process
+        /// is taking place and the name of the native library to resolve as parameters. The delegate should return an OS handle (as IntPtr) for the loaded native library.
+        /// If the delegate returnes <see cref="IntPtr.Zero"/> then the instance of <see cref="ModuleAssemblyLoadContext"/> will try to resolve the native library using the
+        /// default resolution process.
+        /// Can be set to null to return the instance of <see cref="ModuleAssemblyLoadContext"/> to the default native library resolution process.
+        /// </param>
+        /// <example>
+        /// <code>
+        /// public void Load(ILua lua, bool is_serverside, GetILuaFromLuaStatePointer lua_extructor, ModuleAssemblyLoadContext assembly_context)
+        /// {
+        ///     assembly_context.SetCustomNativeLibraryResolver((context, library_name) =>
+        ///     {
+        ///         if(library_name == "some_special_library_for_which_we_want_to_customize_resolution_process")
+        ///         {
+        ///             return System.Runtime.InteropServices.NativeLibrary.Load("path/to/that/special/library");
+        ///         }
+        ///         else
+        ///         {
+        ///             // By returning IntPtr.Zero here we allow GmodNET runtime to use the default resolution process
+        ///             // for libraries with different names.
+        ///             return IntPtr.Zero;
+        ///         }
+        ///     });
+        /// }
+        /// </code>
+        /// </example>
         public abstract void SetCustomNativeLibraryResolver(Func<ModuleAssemblyLoadContext, string, IntPtr> resolver);
     }
 }
