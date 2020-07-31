e9d23f450738b05ef5b86ada8cb4b657eb7fe2d8
GlebChili <gleb.chili@gmail.com>
2020-07-31 22:15:43 +0300

    ITest.Start now takes ModuleAssemblyLoadContext as a parameter

--- gm_dotnet_managed/Tests/ITest.cs
+++ gm_dotnet_managed/Tests/ITest.cs
@@ -8,6 +8,6 @@ namespace Tests
 {
     public interface ITest
     {
-        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor);
+        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor, ModuleAssemblyLoadContext assembly_context);
     }
 }
