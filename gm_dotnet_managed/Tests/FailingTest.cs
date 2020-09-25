17ea39c37b07880ff2dca81880d65cf9fadf3b4b
Gleb Krasilich <gleb.chili@gmail.com>
2020-09-25 16:40:00 +0300

    Failing test added

---
+++ gm_dotnet_managed/Tests/FailingTest.cs
@@ -0,0 +1,21 @@
+﻿using GmodNET.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class FailingTest : ITest
+    {
+        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor, ModuleAssemblyLoadContext assembly_context)
+        {
+            TaskCompletionSource<bool> taskCompletion = new();
+
+            taskCompletion.TrySetException(new Exception[] { new Exception("Test exception") });
+
+            return taskCompletion.Task;
+        }
+    }
+}
