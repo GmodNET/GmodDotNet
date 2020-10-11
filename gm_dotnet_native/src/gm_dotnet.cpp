01761c5de54c82d799cf9658de0fcba3e5bda19c
Gleb Krasilich <gleb.chili@gmail.com>
2020-10-11 10:41:32 +0300

    New load process for Windows

--- gm_dotnet_native/src/gm_dotnet.cpp
+++ gm_dotnet_native/src/gm_dotnet.cpp
@@ -11,6 +11,11 @@
 #include <locale>
 #endif // WIN32
 #include <string>
+#include "../dotnethelper-src/cleanup_function_type.h"
+
+typedef cleanup_function_fn (*InitNetRuntime_fn)(GarrysMod::Lua::ILuaBase* lua);
+
+cleanup_function_fn cleanup_function = nullptr;
 
 //Invoked by Garry's Mod on module load
 GMOD_MODULE_OPEN()
@@ -30,13 +35,48 @@ GMOD_MODULE_OPEN()
     LUA->Call(1, 0);
     LUA->Pop(1);
 
+#ifdef WIN32
+    HMODULE dotnethelper_handle = LoadLibraryA("garrysmod/lua/bin/dotnethelper.dll");
+    InitNetRuntime_fn InitNetRuntime = reinterpret_cast<InitNetRuntime_fn>(GetProcAddress(dotnethelper_handle, "InitNetRuntime"));
+#elif __APPLE__
+
+#elif __gnu_linux__
+
+#endif
+    if(InitNetRuntime == nullptr)
+    {
+        LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+        LUA->GetField(-1, "print");
+        LUA->PushString("::error::Unable to load dotnet helper library.");
+        LUA->Call(1, 0);
+        LUA->Pop(1);
+
+        return 0;
+    }
+
+    cleanup_function = InitNetRuntime(LUA);
+
+    if(cleanup_function == nullptr)
+    {
+        LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
+        LUA->GetField(-1, "print");
+        LUA->PushString("::error::Unable to load dotnet runtime, check standard error stream of the process.");
+        LUA->Call(1, 0);
+        LUA->Pop(1);
+
+        return 0;
+    }
+
     return 0;
 }
 
 //Invoked by Garry's Mod on module unload
 GMOD_MODULE_CLOSE()
 {
-
+    if(cleanup_function != nullptr)
+    {
+        cleanup_function();
+    }
 
     return 0;
 }
