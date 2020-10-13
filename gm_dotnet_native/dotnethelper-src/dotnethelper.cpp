d97a2f85f84f356f2ea17cbc388e6da384f87629
Gleb Krasilich <gleb.chili@gmail.com>
2020-10-14 02:22:58 +0300

    Update dotnethelper.cpp
    .NET 5 RC2 update

--- gm_dotnet_native/dotnethelper-src/dotnethelper.cpp
+++ gm_dotnet_native/dotnethelper-src/dotnethelper.cpp
@@ -34,11 +34,11 @@ std::ofstream error_log_file;
 managed_main_fn managed_main = nullptr;
 
 #ifdef WIN32
-void* hostfxr_library_handle = LoadLibraryA("garrysmod/lua/bin/dotnet/host/fxr/5.0.0-rc.1.20451.14/hostfxr.dll");
+void* hostfxr_library_handle = LoadLibraryA("garrysmod/lua/bin/dotnet/host/fxr/5.0.0-rc.2.20475.17/hostfxr.dll");
 #elif __APPLE__
-void* hostfxr_library_handle = dlopen("garrysmod/lua/bin/dotnet/host/fxr/5.0.0-rc.1.20451.14/libhostfxr.dylib", RTLD_LAZY);
+void* hostfxr_library_handle = dlopen("garrysmod/lua/bin/dotnet/host/fxr/5.0.0-rc.2.20475.17/libhostfxr.dylib", RTLD_LAZY);
 #elif __gnu_linux__
-void* hostfxr_library_handle = dlopen("garrysmod/lua/bin/dotnet/host/fxr/5.0.0-rc.1.20451.14/libhostfxr.so", RTLD_LAZY);
+void* hostfxr_library_handle = dlopen("garrysmod/lua/bin/dotnet/host/fxr/5.0.0-rc.2.20475.17/libhostfxr.so", RTLD_LAZY);
 #endif
 
 #ifdef WIN32
