1af9c06414489f7a57cf5f5fee3c6f4a5b5b92c2
Gleb Krasilich <gleb.chili@gmail.com>
2020-11-09 23:29:33 +0300

    Create SecondPushManagedFunctionTest.cs

---
+++ gm_dotnet_managed/Tests/SecondPushManagedFunctionTest.cs
@@ -0,0 +1,50 @@
+﻿using GmodNET.API;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    // Second PushManagedFunction test. Throwing exception in managed function.
+    public class SecondPushManagedFunctionTest : ITest
+    {
+        string random_string;
+
+        public SecondPushManagedFunctionTest()
+        {
+            random_string = Guid.NewGuid().ToString();
+        }
+
+        public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor, ModuleAssemblyLoadContext assembly_context)
+        {
+            TaskCompletionSource<bool> taskCompletion = new TaskCompletionSource<bool>();
+
+            try
+            {
+                lua.PushManagedFunction((lua) =>
+                {
+                    throw new Exception(random_string);
+                });
+                lua.MCall(0, 0);
+
+                throw new Exception("The managed exception was not caught by MCall.");
+            }
+            catch (Exception e)
+            {
+                if (e is GmodLuaException && e.Message.Contains(random_string))
+                {
+                    taskCompletion.TrySetResult(true);
+                }
+                else
+                {
+                    taskCompletion.TrySetException(new Exception[] { e });
+                }
+            }
+
+            return taskCompletion.Task;
+        }
+    }
+}
