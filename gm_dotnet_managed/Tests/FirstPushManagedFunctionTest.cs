3a063aee0c3f99f3fa1409caf1dd35e777e8aaed
Gleb Krasilich <gleb.chili@gmail.com>
2020-11-09 23:01:00 +0300

    First Task

--- gm_dotnet_managed/Tests/FirstPushManagedFunctionTest.cs
+++ gm_dotnet_managed/Tests/FirstPushManagedFunctionTest.cs
@@ -7,19 +7,44 @@ using System.Threading.Tasks;
 
 namespace Tests
 {
+    // First test for PushManagedFunction
     public class FirstPushManagedFunctionTest : ITest
     {
+        string random_string;
+
+        public FirstPushManagedFunctionTest()
+        {
+            random_string = Guid.NewGuid().ToString();
+        }
+
         public Task<bool> Start(ILua lua, GetILuaFromLuaStatePointer lua_extructor, ModuleAssemblyLoadContext assembly_context)
         {
-            lua.PushSpecial(SPECIAL_TABLES.SPECIAL_GLOB);
-            lua.PushManagedFunction((lua) =>
-            {
-                throw new Exception("Some very random exception");
-            });
-            lua.SetField(-2, "ManagedTestFunc");
-            lua.Pop(1);
+            TaskCompletionSource<bool> taskCompletion = new TaskCompletionSource<bool>();
 
-            return Task.FromResult(true);
+            try
+            {
+                lua.PushManagedFunction((lua) =>
+                {
+                    lua.PushString(random_string);
+                    return 1;
+                });
+                lua.MCall(0, 1);
+                string ret_string = lua.GetString(-1);
+                lua.Pop(1);
+
+                if(ret_string != random_string)
+                {
+                    throw new Exception("Return string is invalid");
+                }
+
+                taskCompletion.TrySetResult(true);
+            }
+            catch(Exception e)
+            {
+                taskCompletion.TrySetException(new Exception[] { e });
+            }
+
+            return taskCompletion.Task;
         }
     }
 }
