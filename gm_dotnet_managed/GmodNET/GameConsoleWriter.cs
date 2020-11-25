456c4bb9bcaabb32d1cf1f736e7c6332fe805978
SupinePandora43 <supinepandoraggg@gmail.com>
2020-11-25 21:41:41 +0500

    disable warning

--- gm_dotnet_managed/GmodNET/GameConsoleWriter.cs
+++ gm_dotnet_managed/GmodNET/GameConsoleWriter.cs
@@ -10,6 +10,7 @@ public class GameConsoleWriter : TextWriter
     public override string NewLine => "\n";
     public override Encoding Encoding => Encoding.UTF8;
 
+#pragma warning disable CA2101
     [DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
     private static extern void Msg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
 
