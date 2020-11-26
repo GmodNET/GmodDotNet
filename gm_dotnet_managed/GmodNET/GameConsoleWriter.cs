f4c383a3161780fbd4d0a7905a86a321c9a7e8b3
SupinePandora43 <supinepandoraggg@gmail.com>
2020-11-26 22:23:14 +0500

    add some of WriteLine methods

--- gm_dotnet_managed/GmodNET/GameConsoleWriter.cs
+++ gm_dotnet_managed/GmodNET/GameConsoleWriter.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -25,5 +27,102 @@ public class GameConsoleWriter : TextWriter
     public override void Write(char[] value)
     {
         Write(new string(value));
+    }
+    public override void Write(char[] buffer, int index, int count)
+    {
+        if (buffer == null)
+        {
+            throw new ArgumentNullException(nameof(buffer));
+        }
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+        if (count < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count));
+        }
+        if (buffer.Length - index < count)
+        {
+            throw new ArgumentException();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < count; i++) stringBuilder.Append(buffer[index + i]);
+        Write(stringBuilder.ToString());
+    }
+    // TODO: it is work?
+    public override void Write(ReadOnlySpan<char> buffer)
+    {
+        Write(new string(buffer));
+    }
+    public override void Write(StringBuilder? value)
+    {
+        if (value != null)
+        {
+            Write(value.ToString());
+        }
+    }
+    // \n begins here
+    public override void WriteLine(string value)
+    {
+        Write(new string(value) + NewLine);
+    }
+    public override void WriteLine(char value)
+    {
+        Write(value.ToString() + NewLine);
+    }
+    public override void WriteLine(char[]? buffer)
+    {
+        Write(new string(buffer) + NewLine);
+    }
+    public override void WriteLine(char[] buffer, int index, int count)
+    {
+        if (buffer == null)
+        {
+            throw new ArgumentNullException(nameof(buffer));
+        }
+        if (index < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(index));
+        }
+        if (count < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count));
+        }
+        if (buffer.Length - index < count)
+        {
+            throw new ArgumentException();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < count; i++) stringBuilder.Append(buffer[index + i]);
+        stringBuilder.Append(NewLine);
+        Write(stringBuilder.ToString());
+    }
+    public override void WriteLine(ReadOnlySpan<char> buffer)
+    {
+        Write(new string(buffer) + NewLine);
+    }
+    public override void WriteLine(StringBuilder? value)
+    {
+        if (value != null)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        else
+        {
+            WriteLine();
+        }
+    }
+    public override void WriteLine(bool value)
+    {
+        Write((value ? "True" : "False") + NewLine);
+    }
+    public override void WriteLine(int value)
+    {
+        Write(value.ToString() + NewLine);
+    }
+    public override void WriteLine(uint value)
+    {
+        Write(value.ToString() + NewLine);
     }
 }
