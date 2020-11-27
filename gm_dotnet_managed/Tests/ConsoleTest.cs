04322f4b3df86ec5f1b7ad000ceba4bffdb86017
SupinePandora43 <supinepandoraggg@gmail.com>
2020-11-27 18:06:30 +0500

    add task test add async await test

--- gm_dotnet_managed/Tests/ConsoleTest.cs
+++ gm_dotnet_managed/Tests/ConsoleTest.cs
@@ -117,6 +117,74 @@ namespace Tests
                 Console.WriteLine((object)null);
                 #endregion
 
+                #region Threaded WriteLine()
+                Task asyncTask = Task.Run(() =>
+                {
+                    // string
+                    Console.WriteLine("string");
+                    Console.WriteLine((string)null);
+                    // bool
+                    Console.WriteLine(true);
+                    Console.WriteLine(false);
+                    // char
+                    Console.WriteLine('c');
+                    // char[]
+                    Console.WriteLine(new char[] { 'c', 'h', 'a', 'r', ' ', 'a', 'r', 'r', 'a', 'y' });
+                    Console.WriteLine((char[])null);
+                    // char[] index length
+                    Console.WriteLine(new char[] { 'c', 'h', 'a', 'r', ' ', 'a', 'r', 'r', 'a', 'y' }, 0, 10);
+                    // ReadOnlySpan<char>
+                    Console.Out.WriteLine(new ReadOnlySpan<char>(new char[] { 'R', 'e', 'a', 'd', 'O', 'n', 'l', 'y', 'S', 'p', 'a', 'n' }));
+                    Console.Out.WriteLine((ReadOnlySpan<char>)null);
+                    // StringBuilder
+                    StringBuilder stringBuilderNewLine = new("Str");
+                    stringBuilderNewLine.Append("ing");
+                    stringBuilderNewLine.Append("Builder");
+                    Console.WriteLine(stringBuilderNewLine);
+                    Console.WriteLine((StringBuilder)null);
+                    // decimal
+                    Console.WriteLine((decimal)1234);
+                    // double
+                    Console.WriteLine((double)1234);
+                    // float
+                    Console.WriteLine((float)1234.56789);
+                    // int
+                    Console.WriteLine(1234);
+                    // uint
+                    Console.WriteLine((uint)1234);
+                    // long
+                    Console.WriteLine(12345678901);
+                    // ulong
+                    Console.WriteLine((ulong)12345678901);
+                    // object
+                    Console.WriteLine(new object());
+                    Console.WriteLine((object)null);
+                });
+                asyncTask.Wait();
+                #endregion
+                #region Async WriteLine()
+                Task asyncAwaitTask = Task.Run(async () =>
+                {
+                    // string
+                    await Console.Out.WriteLineAsync("string");
+                    await Console.Out.WriteLineAsync((string)null);
+                    // char
+                    await Console.Out.WriteLineAsync('c');
+                    // char[]
+                    await Console.Out.WriteLineAsync(new char[] { 'c', 'h', 'a', 'r', ' ', 'a', 'r', 'r', 'a', 'y' });
+                    await Console.Out.WriteLineAsync((char[])null);
+                    // char[] index length
+                    await Console.Out.WriteLineAsync(new char[] { 'c', 'h', 'a', 'r', ' ', 'a', 'r', 'r', 'a', 'y' }, 0, 10);
+                    // StringBuilder
+                    StringBuilder stringBuilderNewLine = new("Str");
+                    stringBuilderNewLine.Append("ing");
+                    stringBuilderNewLine.Append("Builder");
+                    await Console.Out.WriteLineAsync(stringBuilderNewLine);
+                    await Console.Out.WriteLineAsync((StringBuilder)null);
+                });
+                asyncAwaitTask.Wait();
+                #endregion
+
                 taskCompletion.TrySetResult(true);
             }
             catch (Exception e)
