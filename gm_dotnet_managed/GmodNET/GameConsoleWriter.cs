45c3b44ededf14afdae517c82978135bd073617e
SupinePandora43 <supinepandoraggg@gmail.com>
2020-11-27 17:07:56 +0500

    add to namepsace GmodNET

--- gm_dotnet_managed/GmodNET/GameConsoleWriter.cs
+++ gm_dotnet_managed/GmodNET/GameConsoleWriter.cs
@@ -3,163 +3,166 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 
-/// <summary>
-/// Redirects Console's output to game's console
-/// </summary>
-public class GameConsoleWriter : TextWriter
+namespace GmodNET
 {
-    public override string NewLine => "\n";
-    public override Encoding Encoding => Encoding.UTF8;
+    /// <summary>
+    /// Redirects Console's output to game's console
+    /// </summary>
+    public class GameConsoleWriter : TextWriter
+    {
+        public override string NewLine => "\n";
+        public override Encoding Encoding => Encoding.UTF8;
 
 #pragma warning disable CA2101
-    [DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
-    private static extern void Msg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+        [DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void Msg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
 
-    public override void Write(string value)
-    {
-        Msg(new string(value));
-    }
-    public override void Write(char value)
-    {
-        Write(value.ToString());
-    }
-    public override void Write(char[] value)
-    {
-        Write(new string(value));
-    }
-    public override void Write(char[] buffer, int index, int count)
-    {
-        if (buffer == null)
+        public override void Write(string value)
         {
-            throw new ArgumentNullException(nameof(buffer));
+            Msg(new string(value));
         }
-        if (index < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(index));
-        }
-        if (count < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(count));
-        }
-        if (buffer.Length - index < count)
-        {
-            throw new ArgumentException();
-        }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < count; i++) stringBuilder.Append(buffer[index + i]);
-        Write(stringBuilder.ToString());
-    }
-    public override void Write(ReadOnlySpan<char> buffer)
-    {
-        Write(new string(buffer));
-    }
-    public override void Write(StringBuilder value)
-    {
-        if (value != null)
+        public override void Write(char value)
         {
             Write(value.ToString());
         }
-    }
-    // \n begins here
-    public override void WriteLine(string value)
-    {
-        Write(new string(value) + NewLine);
-    }
-    public override void WriteLine(char value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(char[] buffer)
-    {
-        Write(new string(buffer) + NewLine);
-    }
-    public override void WriteLine(char[] buffer, int index, int count)
-    {
-        if (buffer == null)
+        public override void Write(char[] value)
         {
-            throw new ArgumentNullException(nameof(buffer));
+            Write(new string(value));
         }
-        if (index < 0)
+        public override void Write(char[] buffer, int index, int count)
         {
-            throw new ArgumentOutOfRangeException(nameof(index));
+            if (buffer == null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+            if (buffer.Length - index < count)
+            {
+                throw new ArgumentException();
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < count; i++) stringBuilder.Append(buffer[index + i]);
+            Write(stringBuilder.ToString());
         }
-        if (count < 0)
+        public override void Write(ReadOnlySpan<char> buffer)
         {
-            throw new ArgumentOutOfRangeException(nameof(count));
+            Write(new string(buffer));
         }
-        if (buffer.Length - index < count)
+        public override void Write(StringBuilder value)
         {
-            throw new ArgumentException();
+            if (value != null)
+            {
+                Write(value.ToString());
+            }
         }
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < count; i++) stringBuilder.Append(buffer[index + i]);
-        stringBuilder.Append(NewLine);
-        Write(stringBuilder.ToString());
-    }
-    public override void WriteLine(ReadOnlySpan<char> buffer)
-    {
-        Write(new string(buffer) + NewLine);
-    }
-    public override void WriteLine(StringBuilder value)
-    {
-        if (value != null)
+        // \n begins here
+        public override void WriteLine(string value)
+        {
+            Write(new string(value) + NewLine);
+        }
+        public override void WriteLine(char value)
         {
             Write(value.ToString() + NewLine);
         }
-        else
+        public override void WriteLine(char[] buffer)
         {
-            WriteLine();
+            Write(new string(buffer) + NewLine);
         }
-    }
-    public override void WriteLine(bool value)
-    {
-        Write((value ? "True" : "False") + NewLine);
-    }
-    public override void WriteLine(int value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(uint value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(long value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(ulong value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(float value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(double value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(decimal value)
-    {
-        Write(value.ToString() + NewLine);
-    }
-    public override void WriteLine(object value)
-    {
-        if (value == null)
+        public override void WriteLine(char[] buffer, int index, int count)
         {
-            WriteLine();
-        }
-        else
-        {
-            // Call WriteLine(value.ToString), not Write(Object), WriteLine().
-            // This makes calls to WriteLine(Object) atomic.
-            if (value is IFormattable f)
+            if (buffer == null)
             {
-                WriteLine(f.ToString(null, FormatProvider));
+                throw new ArgumentNullException(nameof(buffer));
+            }
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+            if (buffer.Length - index < count)
+            {
+                throw new ArgumentException();
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < count; i++) stringBuilder.Append(buffer[index + i]);
+            stringBuilder.Append(NewLine);
+            Write(stringBuilder.ToString());
+        }
+        public override void WriteLine(ReadOnlySpan<char> buffer)
+        {
+            Write(new string(buffer) + NewLine);
+        }
+        public override void WriteLine(StringBuilder value)
+        {
+            if (value != null)
+            {
+                Write(value.ToString() + NewLine);
             }
             else
             {
-                WriteLine(value.ToString());
+                WriteLine();
+            }
+        }
+        public override void WriteLine(bool value)
+        {
+            Write((value ? "True" : "False") + NewLine);
+        }
+        public override void WriteLine(int value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(uint value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(long value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(ulong value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(float value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(double value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(decimal value)
+        {
+            Write(value.ToString() + NewLine);
+        }
+        public override void WriteLine(object value)
+        {
+            if (value == null)
+            {
+                WriteLine();
+            }
+            else
+            {
+                // Call WriteLine(value.ToString), not Write(Object), WriteLine().
+                // This makes calls to WriteLine(Object) atomic.
+                if (value is IFormattable f)
+                {
+                    WriteLine(f.ToString(null, FormatProvider));
+                }
+                else
+                {
+                    WriteLine(value.ToString());
+                }
             }
         }
     }
